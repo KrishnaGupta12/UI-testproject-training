1d2425afdec00089a98280cbed48953ee10d16e6
KrishnaGupta12 <mail.krishna87868@gmail.com>
2021-09-06 15:36:19 +0530

    UI project first commit

---
+++ src/test/java/com/lti/UIProject/UiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.UIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
